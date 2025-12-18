40c3960a5ae4f78066c1b5fe2a47b31ef31fe96d
Efstathios <modo575@hotmail.com>
2025-12-18 11:00:30 +0100

    add userSWR for clients

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -2,12 +2,27 @@ import TaskList from "@/components/Tasks/TaskList";
 import useSWR from "swr";
 
 export default function DashboardPage() {
-  const { data: tasks, error, isLoading } = useSWR("/api/tasks");
-  if (error) {
+  const {
+    data: tasks,
+    error: tasksError,
+    isLoading: tasksLoading,
+  } = useSWR("/api/tasks");
+  const {
+    data: clients,
+    error: clientsError,
+    isLoading: clientsLoading,
+  } = useSWR("/api/clients");
+  if (tasksError) {
     return <p>Failed to load tasks...</p>;
   }
-  if (isLoading) {
+  if (tasksLoading) {
     return <p>Loading tasks...</p>;
+  }
+  if (clientsError) {
+    return <p>Failed to load clients...</p>;
+  }
+  if (clientsLoading) {
+    return <p>Loading clients...</p>;
   }
   return (
     <>
