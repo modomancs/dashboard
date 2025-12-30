01cd87cd84963ec2e9e52440a8c104d02783cc1c
Efstathios <modo575@hotmail.com>
2025-12-30 11:49:36 +0100

    add status update

--- components/TaskDetails/TaskDetails.js
+++ components/TaskDetails/TaskDetails.js
@@ -1,6 +1,8 @@
 import { ArrowLeft } from "lucide-react";
 import TaskCard from "../Tasks/TaskCard";
 import { useRouter } from "next/router";
+import { mutate } from "swr";
+import { useState } from "react";
 
 export default function TaskDetails({ task, clients, companies }) {
   const router = useRouter();
@@ -8,14 +10,36 @@ export default function TaskDetails({ task, clients, companies }) {
   const company = companies.find((company) => company._id === task.companyId);
 
   const date = new Date(task.createdAt).toLocaleString();
+
+  async function handleStatusUpdate(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const statusData = Object.fromEntries(formData);
+    await fetch(`/api/tasks/${task._id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(statusData),
+    });
+
+    mutate();
+  }
   return (
     <>
       <ArrowLeft onClick={() => router.back()} />
       <TaskCard task={task} clients={clients} />
-      <p>
-        <strong>Status: </strong>
-        {task.status}
-      </p>
+      <EditableItem
+        onSubmit={handleStatusUpdate}
+        display={<p>Status: {task.status}</p>}
+      >
+        <label htmlFor="status">Update status:</label>
+        <select id="status" name="status" defaultValue={task.status}>
+          <option value="todo">To Do</option>
+          <option value="in_progress">In Progress</option>
+          <option value="done">Done</option>
+        </select>
+
+        <button type="submit">Save</button>
+      </EditableItem>
 
       <strong>Description: </strong>
       <p>{task.description}</p>
@@ -30,5 +54,40 @@ export default function TaskDetails({ task, clients, companies }) {
 
       <p>{date}</p>
     </>
+  );
+}
+
+function EditableItem({ children, display, onSubmit }) {
+  const [toggleEdit, setToggleEdit] = useState(false);
+
+  const closeEdit = () => setToggleEdit(false);
+
+  const handleSubmit = async (event) => {
+    if (onSubmit) {
+      await onSubmit(event);
+      closeEdit();
+    }
+  };
+
+  if (toggleEdit) {
+    return (
+      <>
+        <form onSubmit={handleSubmit}>{children}</form>
+
+        <button type="button" onClick={closeEdit}>
+          Cancel
+        </button>
+      </>
+    );
+  }
+
+  return (
+    <div>
+      {display}
+
+      <button type="button" onClick={() => setToggleEdit(true)}>
+        Edit
+      </button>
+    </div>
   );
 }
