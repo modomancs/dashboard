d060c98331c53817c072a22158f293b04fe21946
Efstathios <modo575@hotmail.com>
2026-01-06 14:40:35 +0100

    add sessionProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,19 @@
 import { SWRConfig } from "swr";
 import GlobalStyle from "../styles";
+import { SessionProvider } from "next-auth/react";
 
 const fetcher = (url) => fetch(url).then((response) => response.json());
 
-export default function App({ Component, pageProps }) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   return (
-    <SWRConfig value={{ fetcher }}>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </SWRConfig>
+    <SessionProvider>
+      <SWRConfig value={{ fetcher }}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </SWRConfig>
+    </SessionProvider>
   );
 }
